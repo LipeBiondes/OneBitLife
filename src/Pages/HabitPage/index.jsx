7a0646950d24a8d018e8fb2b10b2ea3bbde7b6ad
Lipe biondes <92653779+LipeBiondes@users.noreply.github.com>
2022-12-01 22:59:33 -0300

    Dropdawn Selecionar Frequencia

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -1,22 +1,24 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 import {
   View,
   Text,
   StyleSheet,
   Image,
   TouchableOpacity,
-  ScrollView,
-} from "react-native";
+  ScrollView
+} from 'react-native'
 
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation } from '@react-navigation/native'
 
-import SelectHabit from "../../Components/HabitPage/SelectHabit";
+import SelectHabit from '../../Components/HabitPage/SelectHabit'
+import SelectFrequency from '../../Components/HabitPage/SelectFrequency'
 
 export default function HabitPage({ route }) {
-	const navigation = useNavigation();
-	const [habitInput, setHabitInput] = useState();
+  const navigation = useNavigation()
+  const [habitInput, setHabitInput] = useState()
+  const [frequencyInput, setFrequencyInput] = useState()
 
-	const { create, habit } = route.params;
+  const { create, habit } = route.params
 
   return (
     <View style={styles.container}>
@@ -27,12 +29,12 @@ export default function HabitPage({ route }) {
             onPress={() => navigation.goBack()}
           >
             <Image
-              source={require("../../assets/icons/arrowBack.png")}
+              source={require('../../assets/icons/arrowBack.png')}
               style={styles.arrowBack}
             />
           </TouchableOpacity>
           <View style={styles.mainContent}>
-            <Text style={styles.title}>Configurações {"\n"} de hábito</Text>
+            <Text style={styles.title}>Configurações {'\n'} de hábito</Text>
             <Text style={styles.inputText}>Área</Text>
             <View style={styles.inputContainer}>
               <Text style={styles.area}>{habit?.habitArea}</Text>
@@ -40,53 +42,59 @@ export default function HabitPage({ route }) {
 
             <Text style={styles.inputText}>Hábito</Text>
             <SelectHabit habit={habit} habitInput={setHabitInput} />
+
+            <Text style={styles.inputText}>Frequência</Text>
+            <SelectFrequency
+              habitFrequency={habit?.habitFrequency}
+              frequencyInput={setFrequencyInput}
+            />
           </View>
         </View>
       </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "rgba(21, 21, 21, 0.98)",
+    backgroundColor: 'rgba(21, 21, 21, 0.98)'
   },
   bakcPageBtn: {
     width: 40,
     height: 40,
-    margin: 25,
+    margin: 25
   },
   arrowBack: {
     width: 40,
-    height: 40,
+    height: 40
   },
   mainContent: {
     width: 250,
-    alignSelf: "center",
+    alignSelf: 'center'
   },
   title: {
-    fontWeight: "bold",
-    textAlign: "center",
-    color: "white",
-    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 30
   },
   inputText: {
-    color: "white",
+    color: 'white',
     fontSize: 16,
     marginTop: 35,
     marginBottom: 10,
-    marginLeft: 5,
+    marginLeft: 5
   },
   inputContainer: {
     borderWidth: 1,
-    borderColor: "#FFFFFF",
+    borderColor: '#FFFFFF',
     borderRadius: 10,
     paddingHorizontal: 20,
-    paddingVertical: 15,
+    paddingVertical: 15
   },
   area: {
-    color: "#BBBBBB",
-    fontSize: 15,
-  },
-});
+    color: '#BBBBBB',
+    fontSize: 15
+  }
+})
